0d25d509dd4ffed647f5b6742079af9fdc89b413
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-08 17:41:59 +0000

    change order by cashpurchases

--- lib/main_pages/cash_purchases/cash_purchases_widget.dart
+++ lib/main_pages/cash_purchases/cash_purchases_widget.dart
@@ -179,18 +179,16 @@ class _CashPurchasesWidgetState extends State<CashPurchasesWidget> {
               Expanded(
                 child: StreamBuilder<List<CashPurchasesRecord>>(
                   stream: queryCashPurchasesRecord(
-                    queryBuilder: (cashPurchasesRecord) => cashPurchasesRecord
-                        .where(
-                          'departmentReference',
-                          isEqualTo: FFAppState()
+                    queryBuilder: (cashPurchasesRecord) =>
+                        cashPurchasesRecord.where(
+                      'departmentReference',
+                      isEqualTo:
+                          FFAppState().departmentAppState.departmentReference,
+                      isNull: (FFAppState()
                               .departmentAppState
-                              .departmentReference,
-                          isNull: (FFAppState()
-                                  .departmentAppState
-                                  .departmentReference) ==
-                              null,
-                        )
-                        .orderBy('date', descending: true),
+                              .departmentReference) ==
+                          null,
+                    ),
                   ),
                   builder: (context, snapshot) {
                     // Customize what your widget looks like when it's loading.
